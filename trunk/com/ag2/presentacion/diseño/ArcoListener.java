e6f6e48c44c7f620f18026e756105e6f3b54586b
yeavella@gmail.com <yeavella@gmail.com@2d66d220-3702-eadc-dac1-119fe40fcf4a>
2011-12-23 05:25:57 +0000

    Eliminando la representacion grafica de los puertos.

--- trunk/com/ag2/presentacion/diseño/ArcoListener.java
+++ trunk/com/ag2/presentacion/diseño/ArcoListener.java
@@ -1,15 +1,7 @@
-/*
- * To change this template, choose Tools | Templates and open the template in
- * the editor.
- */
 package com.ag2.presentacion.diseño;
 
-/**
- *
- * @author Frank
- */
 public interface ArcoListener 
 {
-     public void update(); 
+     public void updateArco(); 
     
 }
