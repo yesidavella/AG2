7e2c563c0f3ca138fb98f0bfbaa90a56dceaf37e
franklinalwinmartinez <franklinalwinmartinez@gmail.com>
2011-12-27 03:20:59 +0000

    MVC Cliente

--- com/ag2/controlador/ControladorAdminNodo.java
+++ com/ag2/controlador/ControladorAdminNodo.java
@@ -71,11 +71,11 @@ public class ControladorAdminNodo extends ControladorAbstractoAdminNodo {
 
             //===========================================================================================================
             PropiedadNodoDistribuciones distribucionesTamanoTrabajo = new PropiedadNodoDistribuciones("generacionTamañoTrabajo", "Generación del tamaño del trabajo");
-            crearPropiedadDistriducion(clientNode.getState().getMaxDelayInterval(), propiedadeNodos, distribucionesTamanoTrabajo, "generacionTamañoTrabajo");
+            crearPropiedadDistriducion(clientNode.getState().getSizeDistribution(), propiedadeNodos, distribucionesTamanoTrabajo, "generacionTamañoTrabajo");
 
             //===========================================================================================================
             PropiedadNodoDistribuciones distribucionesTamanoRespuesta = new PropiedadNodoDistribuciones("generacionTamañoRespuesta", "Generación del tamaño de la respuesta");
-            crearPropiedadDistriducion(clientNode.getState().getMaxDelayInterval(), propiedadeNodos, distribucionesTamanoRespuesta, "generacionTamañoRespuesta");
+            crearPropiedadDistriducion(clientNode.getState().getAckSizeDistribution(), propiedadeNodos, distribucionesTamanoRespuesta, "generacionTamañoRespuesta");
 
 
         }
@@ -182,90 +182,158 @@ public class ControladorAdminNodo extends ControladorAbstractoAdminNodo {
     }
 
     @Override
-    public void updatePropiedad(String id, String valor)
-    {
-        System.out.println("prop control " + id +" "+valor);
+    public void updatePropiedad(String id, String valor) {
+        System.out.println("prop control " + id + " " + valor);
 
-        if (id.equalsIgnoreCase("nombre")) 
-        {
+        if (id.equalsIgnoreCase("nombre")) {
             nodoGraficoSeleccionado.setNombre(valor);
             //clientNode.setID(valor);
         }
-        
-        if (nodoGraficoSeleccionado instanceof NodoClienteGrafico)
+
+        if (nodoGraficoSeleccionado instanceof NodoClienteGrafico) 
         {
             ClientNode clientNode = (ClientNode) parejasDeNodosExistentes.get(nodoGraficoSeleccionado);
-            if(id.equalsIgnoreCase("generacionTrabajos"))
+            
+            if (id.equalsIgnoreCase("generacionTrabajos"))
             {
-                clientNode.getState().setJobInterArrival(getDistributionByText(valor)); 
+                clientNode.getState().setJobInterArrival(getDistributionByText(valor));
+                consultarPropiedades(nodoGraficoSeleccionado);
+            }
+            else if (id.contains("generacionTrabajos")) 
+            {
+                setValuesDistribution(clientNode.getState().getJobInterArrival(), valor, id);
+            }
+            else if (id.equalsIgnoreCase("generacionFlops"))
+            {
+                clientNode.getState().setFlops(getDistributionByText(valor));
+                consultarPropiedades(nodoGraficoSeleccionado);
+            } 
+            else if (id.contains("generacionFlops"))
+            {
+                setValuesDistribution(clientNode.getState().getFlops(), valor, id);                
+            } 
+            else if (id.equalsIgnoreCase("generacionMaximoRetraso")) 
+            {
+                clientNode.getState().setMaxDelayInterval(getDistributionByText(valor));
                 consultarPropiedades(nodoGraficoSeleccionado);                
             }
-            else if(id.contains("generacionTrabajos")) 
-            {               
-                  if(id.contains("DDErlang"))
-                  {
-                      DDErlang dDErlang = (DDErlang)clientNode.getState().getJobInterArrival();
-                      
-                      if(id.contains("Orden"))
-                      {
-                           dDErlang.setN(Integer.parseInt(valor));
-                      }
-                      else if(id.contains("Promedio"))
-                      {
-                           dDErlang.setAvg(Double.parseDouble(valor));
-                      }                  
-                  } 
-                  else if(id.contains("DDHyperExp"))
-                  {
-                      DDHyperExp dDHyperExp = (DDHyperExp)clientNode.getState().getJobInterArrival();
-                      
-                      if(id.contains("Lamdas"))
-                      {
-                          
-                           
-                      }
-                      else if(id.contains("Oportunidades"))
-                      {
-                          
-                      }                   
-                      
-                  }
+            else if (id.contains("generacionMaximoRetraso")) 
+            {
+                setValuesDistribution(clientNode.getState().getMaxDelayInterval(), valor, id);
+                
+            } else if (id.equalsIgnoreCase("generacionTamañoTrabajo")) 
+            {
+                clientNode.getState().setSizeDistribution(getDistributionByText(valor));
+                consultarPropiedades(nodoGraficoSeleccionado);
+                
+            } else if (id.contains("generacionTamañoTrabajo")) 
+            {
+                setValuesDistribution(clientNode.getState().getSizeDistribution(), valor, id);
+            } 
+            else if (id.equalsIgnoreCase("generacionTamañoRespuesta")) 
+            {
+                clientNode.getState().setAckSizeDistribution(getDistributionByText(valor));
+                consultarPropiedades(nodoGraficoSeleccionado);
+                
+            } else if (id.contains("generacionTamañoRespuesta"))
+            {
+                setValuesDistribution(clientNode.getState().getAckSizeDistribution(), valor, id);
             }
-        }  
+
+
+
+
+        }
     }
-    
-    private DiscreteDistribution getDistributionByText(String value)
-    {
-        if(value.equals("Uniforme"))
-        {
+
+    private void setValuesDistribution(DiscreteDistribution distribution, String value, String id) {
+        if (id.contains("DDErlang")) {
+
+            DDErlang dDErlang = (DDErlang) distribution;
+
+            if (id.contains("Orden")) {
+                dDErlang.setN(Integer.parseInt(value));
+            } else if (id.contains("Promedio")) {
+                dDErlang.setAvg(Double.parseDouble(value));
+            }
+        } else if (id.contains("DDHyperExp")) {
+            DDHyperExp dDHyperExp = (DDHyperExp) distribution;
+
+            String[] values = value.split("-");
+            double valuesDoubles[] = new double[values.length];
+            for (int i = 0; i < values.length; i++) {
+                valuesDoubles[i] = Double.parseDouble(values[i]);
+            }
+
+            if (id.contains("Lamdas")) {
+                dDHyperExp.setLambdas(valuesDoubles);
+
+            } else if (id.contains("Oportunidades")) {
+                dDHyperExp.setChances(valuesDoubles);
+            }
+
+        } else if (id.contains("DDNegExp")) {
+            DDNegExp dDNegExp = (DDNegExp) distribution;
+
+            if (id.contains("Promedio")) {
+                dDNegExp.setAvg(Double.parseDouble(value));
+            }
+
+        } else if (id.contains("DDNormal")) {
+            DDNormal dDNormal = (DDNormal) distribution;
+
+            if (id.contains("Promedio")) {
+                dDNormal.setAvg(Double.parseDouble(value));
+            } else if (id.contains("DesviacionEstandar")) {
+                dDNormal.setDev(Double.parseDouble(value));
+            }
+
+        } else if (id.contains("DDPoissonProcess")) {
+            DDPoissonProcess dDPoissonProcess = (DDPoissonProcess) distribution;
+            if (id.contains("Promedio")) {
+                dDPoissonProcess.setAverage(Double.parseDouble(value));
+            }
+        } else if (id.contains("DDUniform")) {
+            DDUniform dDUniform = (DDUniform) distribution;
+
+            if (id.contains("Minimo")) {
+                dDUniform.setMin(Double.parseDouble(value));
+            } else if (id.contains("Maximo")) {
+                dDUniform.setMax(Double.parseDouble(value));
+            }
+
+        } else if (id.contains("ConstantDistribution")) {
+            ConstantDistribution constantDistribution = (ConstantDistribution) distribution;
+
+            if (id.contains("Constante")) {
+                constantDistribution.setConstant(Double.parseDouble(value));
+            }
+        }
+
+
+
+
+
+    }
+
+    private DiscreteDistribution getDistributionByText(String value) {
+        if (value.equals("Uniforme")) {
             return new DDUniform(10, 20);
-        }  
-        else if(value.equals("Constante"))
-        {
+        } else if (value.equals("Constante")) {
             return new ConstantDistribution(10);
-        }
-        else if(value.equals("Erlang"))
-        {
+        } else if (value.equals("Erlang")) {
             return new DDErlang(1, 2);
-        }
-        else if(value.equals("Hiper-exponencial"))
-        {
-            return new DDHyperExp(new double[]{1,2,3}, new double[]{1,2,3});
-        }
-        else if(value.equals("Exponencial-negativa"))
-        {
+        } else if (value.equals("Hiper-exponencial")) {
+            return new DDHyperExp(new double[]{1, 2, 3}, new double[]{1, 2, 3});
+        } else if (value.equals("Exponencial-negativa")) {
             return new DDNegExp(2);
-        }
-        else if(value.equals("Normal"))
-        {
-            return new DDNormal(2,3);            
-        }
-        else if(value.equals("Possion"))
-        {
+        } else if (value.equals("Normal")) {
+            return new DDNormal(2, 3);
+        } else if (value.equals("Possion")) {
             return new DDPoissonProcess(2);
         }
-               
+
         return null;
-        
+
     }
 }
