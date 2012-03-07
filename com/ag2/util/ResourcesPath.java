33bead4d49b7d532a9566e864552d62d1db927dd
yeavella <yeavella@gmail.com>
2012-03-07 02:31:46 +0000

    Ejecutar aplicativo desde jar, jnpl y navegador web terminado.

--- com/ag2/util/ResourcesPath.java
+++ com/ag2/util/ResourcesPath.java
@@ -7,5 +7,4 @@ public class ResourcesPath {
     public final static String ABS_PATH_CONFIG_AG2 = URLUtil.fileToURL(new File("resources/")).toString().replaceFirst("file:/","");
     public final static String ABS_PATH_IMGS = URLUtil.fileToURL(new File("resources/images/")).toString();
     public final static String ABS_PATH_CSS = URLUtil.fileToURL(new File("resources/css/")).toString();
-    String a ="aa";
 }
