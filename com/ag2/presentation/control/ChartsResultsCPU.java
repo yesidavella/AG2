3e0f20fc251b12a92af5e7d6c7159d10611659ce
franklinalwinmartinez <franklinalwinmartinez@gmail.com>
2012-04-16 04:15:12 +0000

    Adicion de datos en los chart

--- com/ag2/presentation/control/ChartsResultsCPU.java
+++ com/ag2/presentation/control/ChartsResultsCPU.java
@@ -15,19 +15,23 @@ import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
-import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.control.Separator;
 import javafx.scene.control.Tab;
-import javafx.scene.effect.DropShadow;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.util.Duration;
 
 /**
@@ -37,14 +41,14 @@ import javafx.util.Duration;
 public class ChartsResultsCPU {
 
     private Tab tab;
-    private VBox hBox;
+    private VBox vBox;
     private transient Timeline time = new Timeline();
     private ScrollPane scrollPane;
     private LineChart<Number, Number> lineChart;
     private HashMap<GraphNode, Entity> nodeMatchCoupleObjectContainer = MatchCoupleObjectContainer.getInstanceNodeMatchCoupleObjectContainer();
     private HashMap<ResourceGraphNode, XYChart.Series<Number, Number>> relationResourceSerie = new HashMap<ResourceGraphNode, XYChart.Series<Number, Number>>();
     private DataChartResourceController dataChartResourceController;
-    private boolean loadResources = true;
+//    private boolean loadResources = true;
     private int countClients = 0;
     private HashMap<ResourceGraphNode, UtilSerieAverage> relationResourceDataAverage = new HashMap<ResourceGraphNode, UtilSerieAverage>();
     private int countPlays = 1;
@@ -53,13 +57,12 @@ public class ChartsResultsCPU {
         this.tab = tab;
         scrollPane = new ScrollPane();
         dataChartResourceController = new DataChartResourceController();
-        hBox = new VBox();
-        hBox.setAlignment(Pos.CENTER);
-        hBox.setPadding(new Insets(10));
-
-        //createChart();
-
-        scrollPane.setContent(hBox);
+        vBox = new VBox();
+        vBox.setAlignment(Pos.CENTER);
+        vBox.setPadding(new Insets(10, 10, 10, 10));
+        vBox.setSpacing(5);
+        vBox.setFillWidth(true);
+        scrollPane.setContent(vBox);
         tab.setContent(scrollPane);
     }
 
@@ -76,28 +79,27 @@ public class ChartsResultsCPU {
 
 
 
+        for (GraphNode graphNode : nodeMatchCoupleObjectContainer.keySet()) {
+            if (graphNode instanceof ResourceGraphNode) {
+                XYChart.Series<Number, Number> serie = new XYChart.Series<Number, Number>();
+                serie.setName(graphNode.toString());
+                serie.getData().add(new XYChart.Data<Number, Number>(0, 0));
+                lineChart.getData().addAll(serie);
+
+                relationResourceSerie.put((ResourceGraphNode) graphNode, serie);
+                relationResourceDataAverage.put((ResourceGraphNode) graphNode, new UtilSerieAverage());
+            }
+            if (graphNode instanceof ClientGraphNode) {
+                countClients++;
+            }
+        }
+
 
         KeyFrame keyFrame = new KeyFrame(Duration.millis(100), new EventHandler<ActionEvent>() {
 
+            @Override
             public void handle(ActionEvent event) {
-                if (loadResources) {
-                    loadResources = false;
-                    for (GraphNode graphNode : nodeMatchCoupleObjectContainer.keySet()) {
-                        if (graphNode instanceof ResourceGraphNode) {
-                            XYChart.Series<Number, Number> serie = new XYChart.Series<Number, Number>();
-                            serie.setName(graphNode.toString());
-                            serie.getData().add(new XYChart.Data<Number, Number>(0, 0));
-                            lineChart.getData().addAll(serie);
-                          
-                            relationResourceSerie.put((ResourceGraphNode) graphNode, serie);
-                            relationResourceDataAverage.put((ResourceGraphNode) graphNode, new UtilSerieAverage());
-                        }
-                        if (graphNode instanceof ClientGraphNode) {
-                            countClients++;
-                        }
 
-                    }
-                }
 
                 for (ResourceGraphNode resourceGraphNode : relationResourceSerie.keySet()) {
 
@@ -108,9 +110,9 @@ public class ChartsResultsCPU {
                         utilSerieAverage.countAverage++;
                         utilSerieAverage.timeAverage += dataChartResourceController.getTime();
                         utilSerieAverage.valueAverage += dataChartResourceController.getValue1();
-                        if (utilSerieAverage.countAverage >= 4 * countClients) {
+                        if (utilSerieAverage.countAverage >= 5 * countClients) {
 
-                            XYChart.Series<Number, Number> serie = relationResourceSerie.get(resourceGraphNode);                     
+                            XYChart.Series<Number, Number> serie = relationResourceSerie.get(resourceGraphNode);
                             serie.getData().add(new XYChart.Data<Number, Number>(utilSerieAverage.timeAverage / utilSerieAverage.countAverage, utilSerieAverage.valueAverage / utilSerieAverage.countAverage));
                             utilSerieAverage.timeAverage = 0;
                             utilSerieAverage.valueAverage = 0;
@@ -127,13 +129,97 @@ public class ChartsResultsCPU {
         });
         time.getKeyFrames().add(keyFrame);
         time.setCycleCount(Timeline.INDEFINITE);
-        lineChart.setEffect(new DropShadow());
-        hBox.getChildren().add(0, lineChart);
+
+        GridPane gridPane = loadInfoCPUResource();
+
+        HBox hBox = new HBox();
+        hBox.getStyleClass().add("boxChart");
+        hBox.setFillHeight(true);
+        lineChart.setEffect(null);
+        hBox.setAlignment(Pos.CENTER);
+        hBox.setPadding(new Insets(5, 5, 5, 5));
+        HBox.setHgrow(gridPane, Priority.ALWAYS);
+        hBox.getChildren().addAll(gridPane, lineChart);
+        hBox.setMinWidth(1200);
+        vBox.getChildren().add(0, hBox);
+    }
+
+    private GridPane loadInfoCPUResource() {
+        GridPane gridPane = new GridPane();
+        gridPane.setVgap(2);
+        gridPane.setHgap(4);
+
+        gridPane.setAlignment(Pos.CENTER);
+
+
+        Label lblNameTitle = new Label("Cluster");
+        Label lblcapacityCPUTitle = new Label("Flops C/U");
+        Label lblcountCPUTitle = new Label("N.CPU");
+
+        lblNameTitle.setTextAlignment(TextAlignment.CENTER);
+        lblcapacityCPUTitle.setTextAlignment(TextAlignment.CENTER);
+        lblcountCPUTitle.setTextAlignment(TextAlignment.CENTER);
+
+        lblNameTitle.setFont(Font.font("Arial", FontWeight.BOLD, 10));
+        lblcapacityCPUTitle.setFont(Font.font("Arial", FontWeight.BOLD, 10));
+        lblcountCPUTitle.setFont(Font.font("Arial", FontWeight.BOLD, 10));
+
+
+
+        GridPane.setConstraints(lblNameTitle, 0, 0);
+        GridPane.setHalignment(lblNameTitle, HPos.LEFT);
+
+        GridPane.setConstraints(lblcountCPUTitle, 1, 0);
+        GridPane.setHalignment(lblcountCPUTitle, HPos.CENTER);
+
+        GridPane.setConstraints(lblcapacityCPUTitle, 2, 0);
+        GridPane.setHalignment(lblcapacityCPUTitle, HPos.CENTER);
+
+        gridPane.getChildren().addAll(lblNameTitle, lblcountCPUTitle, lblcapacityCPUTitle);
+        int row = 1;
+
+
+        for (ResourceGraphNode resourceGraphNode : relationResourceSerie.keySet()) {
+            dataChartResourceController.loadInfoCPUResouce(resourceGraphNode);
+            String name = resourceGraphNode.getName();
+            String capacityCPU = String.valueOf(dataChartResourceController.getCapacityCPU());
+            String countCPU = String.valueOf(dataChartResourceController.getCountCPU());
+
+            Label lblName = new Label(name);
+            Label lblcapacityCPU = new Label(capacityCPU);
+            Label lblcountCPU = new Label(countCPU);
+
+            lblName.setFont(Font.font("Arial", FontWeight.NORMAL, 10));
+            lblcapacityCPU.setFont(Font.font("Arial", FontWeight.NORMAL, 10));
+            lblcountCPU.setFont(Font.font("Arial", FontWeight.NORMAL, 10));
+
+            lblName.setTextAlignment(TextAlignment.CENTER);
+            lblcapacityCPU.setTextAlignment(TextAlignment.CENTER);
+            lblcountCPU.setTextAlignment(TextAlignment.CENTER);
+
+            GridPane.setHalignment(lblName, HPos.CENTER);
+            GridPane.setHalignment(lblcapacityCPU, HPos.CENTER);
+            GridPane.setHalignment(lblcountCPU, HPos.CENTER);
+
+
+            GridPane.setConstraints(lblName, 0, row);
+            GridPane.setConstraints(lblcountCPU, 1, row);
+            GridPane.setConstraints(lblcapacityCPU, 2, row);
+
+            gridPane.getChildren().addAll(lblName, lblcountCPU, lblcapacityCPU);
+
+            row++;
+        }
+        int rowHeight = (row * 14) + 14;
+        gridPane.setMinHeight(rowHeight);
+        gridPane.setMaxHeight(rowHeight);
+        gridPane.setPrefHeight(rowHeight);
+        return gridPane;
     }
 
     public void play() {
         createChart();
-        loadResources = true;
+
         time.play();
     }
 
@@ -143,12 +229,12 @@ public class ChartsResultsCPU {
         for (ResourceGraphNode resourceGraphNode : relationResourceSerie.keySet()) {
 
             UtilSerieAverage utilSerieAverage = relationResourceDataAverage.get(resourceGraphNode);
-
+            XYChart.Series<Number, Number> serie = relationResourceSerie.get(resourceGraphNode);
             if (!utilSerieAverage.lastData) {
-                XYChart.Series<Number, Number> serie = relationResourceSerie.get(resourceGraphNode);
                 serie.getData().add(new XYChart.Data<Number, Number>(100, utilSerieAverage.valueAverage / utilSerieAverage.countAverage));
             } else {
-                System.out.println("  else fin time " + (utilSerieAverage.timeAverage / utilSerieAverage.countAverage) + " value " + (utilSerieAverage.valueAverage / utilSerieAverage.countAverage));
+                double repeatValue = serie.getData().get(serie.getData().size() - 1).getYValue().doubleValue();
+                serie.getData().add(new XYChart.Data<Number, Number>(100, repeatValue));
             }
 
         }
@@ -160,7 +246,7 @@ public class ChartsResultsCPU {
         private double timeAverage = 0;
         private double valueAverage = 0;
         private boolean lastData = true;
-       
+
         public int getCountAverage() {
             return countAverage;
         }
