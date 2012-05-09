a83c49aaf56bd6642d4a1a9ce86fc23d96bf0f39
yeavella <yeavella@gmail.com>
2012-05-09 02:54:10 +0000

    AL parecer ya serializa y des-serializa bien.

--- com/ag2/model/GridSimulatorModel.java
+++ com/ag2/model/GridSimulatorModel.java
@@ -4,6 +4,7 @@ import Grid.Routing.RoutingViaJung;
 import Grid.Routing.ShortesPathRouting;
 import com.ag2.config.PropertyPhosphorusTypeEnum;
 import com.ag2.controller.ResultsAbstractController;
+import java.io.ObjectInputStream;
 import simbase.Exceptions.StopException;
 import simbase.Port.SimBaseInPort;
 import simbase.SimBaseEntity;
@@ -20,13 +21,13 @@ public class GridSimulatorModel extends Grid.GridSimulator {
     private ResultsAbstractController resultsAbstractController;
 
     public GridSimulatorModel() {
-        
-        if(PropertyPhosphorusTypeEnum.getBooleanProperty(PropertyPhosphorusTypeEnum.ROUTED_VIA_JUNG)){
+
+        if (PropertyPhosphorusTypeEnum.getBooleanProperty(PropertyPhosphorusTypeEnum.ROUTED_VIA_JUNG)) {
             super.setRouting(new RoutingViaJung(this));
-        }else{
+        } else {
             super.setRouting(new ShortesPathRouting(this));
         }
-                
+
         simulationTime = PropertyPhosphorusTypeEnum.getDoubleProperty(PropertyPhosphorusTypeEnum.SIMULATION_TIME);
     }
 
@@ -37,12 +38,10 @@ public class GridSimulatorModel extends Grid.GridSimulator {
     public void setViewResultsPhosphorus(ResultsAbstractController resultsAbstractController) {
         this.resultsAbstractController = resultsAbstractController;
     }
-   
 
     @Override
     public boolean runNextEvent() throws StopException {
-        if (events.isEmpty())
-        {
+        if (events.isEmpty()) {
             System.out.println("No hay ams eventos¡¡¡");
             throw new StopException("There are no events to execute anymore");
         }
@@ -61,18 +60,32 @@ public class GridSimulatorModel extends Grid.GridSimulator {
 
         countEventToAdvise++;
 
-        if(countEventToAdvise>10)
-        {
-            countEventToAdvise=0;
-            resultsAbstractController.setExecutionPercentage(percentage,masterClock.getTime());
-        }        
-        
+        if (countEventToAdvise > 10) {
+            countEventToAdvise = 0;
+            resultsAbstractController.setExecutionPercentage(percentage, masterClock.getTime());
+        }
+
         percentageShort = Math.round((float) percentage);
         if (percentageShort != percentageSwapShort) {
             percentageSwapShort = percentageShort;
-            resultsAbstractController.setExecutionPercentage(percentage,masterClock.getTime());
+            resultsAbstractController.setExecutionPercentage(percentage, masterClock.getTime());
 
         }
         return true;
+    }
+
+    private void readObject(ObjectInputStream inputStream) {
+        try {
+            inputStream.defaultReadObject();
+
+            if (PropertyPhosphorusTypeEnum.getBooleanProperty(PropertyPhosphorusTypeEnum.ROUTED_VIA_JUNG)) {
+                super.setRouting(new RoutingViaJung(this));
+            } else {
+                super.setRouting(new ShortesPathRouting(this));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
