658d9e7a21dbe59f7e7bc8881b0e77cf5c9a0d63
yeavella@gmail.com <yeavella@gmail.com@2d66d220-3702-eadc-dac1-119fe40fcf4a>
2012-07-04 05:26:16 +0000

    Faltan tamaños a los charts, practicamente terminados.

--- trunk/com/ag2/presentation/control/ChartsResultsSwitch.java
+++ trunk/com/ag2/presentation/control/ChartsResultsSwitch.java
@@ -15,6 +15,7 @@ import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.StackedBarChart;
 import javafx.scene.chart.XYChart;
+import javafx.scene.chart.XYChart.Data;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.Tab;
 import javafx.scene.layout.HBox;
@@ -31,22 +32,15 @@ public class ChartsResultsSwitch implements ViewResultsSwitchChart {
     private StackedBarChart<String, Number> barChart;
     private CategoryAxis xAxis;
     private NumberAxis yAxis;
-    private XYChart.Series<String, Number> uniqueSerie;
-//    private XYChart.Series<String, Number> serieNoFreeResouce;
-    private XYChart.Series<String, Number> serieReqAckSent;
-//    private XYChart.Series<String, Number> serieSendingFailed;
-//    private XYChart.Series<String, Number> serieResultsSwiched;
-//    private XYChart.Series<String, Number> serieResultsNoSwitched;
+    private XYChart.Series<String, Number> serieJobsSwitched;
+    private XYChart.Series<String, Number> serieJobsNoSwitched;
+    private XYChart.Series<String, Number> serieResultsSwiched;
+    private XYChart.Series<String, Number> serieResultsNoSwitched;
+    private XYChart.Series<String, Number> serieRequestsSwitched;
+    private XYChart.Series<String, Number> serieRequestsNoSwitched;
     private int countPlays = 1;
-    
-        final static String austria = "Austria";
-    final static String brazil = "Brazil";
-    final static String france = "France";
-    final static String italy = "Italy";
-    final static String usa = "USA";
-    
     ArrayList<SimBaseEntity> switchesList;
-        ArrayList<String> switchesNameList;
+    ArrayList<String> switchesNameList;
 
     public ChartsResultsSwitch(Tab tabChartsSwitchResults) {
         this.tabChartsSwitchResults = tabChartsSwitchResults;
@@ -56,42 +50,51 @@ public class ChartsResultsSwitch implements ViewResultsSwitchChart {
         vbxAllPage.setSpacing(10);
         scpAllPage.setContent(vbxAllPage);
         tabChartsSwitchResults.setContent(scpAllPage);
-        
+
     }
 
     public void play() {
-        
-        
+
         switchesList = SimulationBase.getInstance().getGridSimulatorModel().getEntitiesOfType(HybridSwitchImpl.class);
-        
         switchesNameList = new ArrayList<String>();
-        
-        for (SimBaseEntity entity:switchesList) {
-            switchesNameList.add(entity.getId());
+
+        for (SimBaseEntity entity : switchesList) {
+            switchesNameList.add(Utils.findGraphicalName(entity.getId()));
         }
-        
+
         xAxis = new CategoryAxis();
         yAxis = new NumberAxis();
-        xAxis.setCategories(FXCollections.observableArrayList(switchesNameList));
 
-//        xAxis.setLabel("Nombre Conmutadores");
-//        yAxis.setLabel("titulo en y");
+        xAxis.setLabel("Nombre Conmutadores");
+        yAxis.setLabel("Mensajes");
+        
         barChart = new StackedBarChart<String, Number>(xAxis, yAxis);
-//        barChart.setTitle("Trafico en los enrutadores. Ejecución numero:" + countPlays);
+        barChart.setTitle("Trafico en los enrutadores. Ejecución numero:" + countPlays);
         countPlays++;
 
+        xAxis.setCategories(FXCollections.observableArrayList(switchesNameList));
+        yAxis.setTickLabelFormatter(new NumberAxis.DefaultFormatter(yAxis));
+        yAxis.setTickLabelsVisible(true);
+        
         hbxPerExecution = new HBox(10);
         hbxPerExecution.getChildren().addAll(barChart);
         hbxPerExecution.getStyleClass().add("boxChart");
         vbxAllPage.getChildren().add(0, hbxPerExecution);
 
-        uniqueSerie = new XYChart.Series<String, Number>();
-        serieReqAckSent = new XYChart.Series<String, Number>();
-//        uniqueSerie.setName("");
+        serieJobsSwitched = new XYChart.Series<String, Number>();
+        serieJobsNoSwitched = new XYChart.Series<String, Number>();
+        serieResultsSwiched = new XYChart.Series<String, Number>();
+        serieResultsNoSwitched = new XYChart.Series<String, Number>();
+        serieRequestsSwitched = new XYChart.Series<String, Number>();
+        serieRequestsNoSwitched = new XYChart.Series<String, Number>();
 
-
+        serieJobsSwitched.setName("Trabajos Conmutados");
+        serieJobsNoSwitched.setName("Trabajos No Conmutados");
+        serieResultsSwiched.setName("Trabs. Atendidos Conmutados");
+        serieResultsNoSwitched.setName("Trabs. Atendidos No Conmutados");
+        serieRequestsSwitched.setName("Solic. de Agen. Conmutadas");
+        serieRequestsNoSwitched.setName("Solic. de Agen. No Conmutadas");
         
-        System.out.print("Play¡¡¡");
     }
 
     @Override
@@ -102,17 +105,24 @@ public class ChartsResultsSwitch implements ViewResultsSwitchChart {
 
         String graficalName = Utils.findGraphicalName(switchName);
 
-//        switchesList.add(graficalName);
-        
-        uniqueSerie.getData().add(new XYChart.Data<String, Number>(switchName, jobsSwitched));
-        serieReqAckSent.getData().add(new XYChart.Data<String, Number>(switchName,resultsSwiched));
-        
+        Data<String, Number> dataJobsSwitched = new XYChart.Data<String, Number>(graficalName, jobsSwitched);
+        Data<String, Number> datajobsNoSwitched = new XYChart.Data<String, Number>(graficalName, jobsNoSwitched);
+        Data<String, Number> dataresultsSwiched = new XYChart.Data<String, Number>(graficalName, resultsSwiched);
+        Data<String, Number> dataresultsNoSwitched = new XYChart.Data<String, Number>(graficalName, resultsNoSwitched);
+        Data<String, Number> datarequestsSwitched = new XYChart.Data<String, Number>(graficalName, requestsSwitched);
+        Data<String, Number> dataRequestsNoSwitched = new XYChart.Data<String, Number>(graficalName, requestsNoSwitched);
 
-        if(barChart.getData().isEmpty()){
-            barChart.getData().addAll(uniqueSerie,serieReqAckSent);
-            System.out.print("Es vacio¡¡¡¡¡¡¡¡¡");
+        serieJobsSwitched.getData().addAll(dataJobsSwitched);
+        serieJobsNoSwitched.getData().addAll(datajobsNoSwitched);
+        serieResultsSwiched.getData().addAll(dataresultsSwiched);
+        serieResultsNoSwitched.getData().addAll(dataresultsNoSwitched);
+        serieRequestsSwitched.getData().addAll(datarequestsSwitched);
+        serieRequestsNoSwitched.getData().addAll(dataRequestsNoSwitched);
+
+        if (barChart.getData().isEmpty()) {
+            barChart.getData().addAll(serieRequestsSwitched, serieRequestsNoSwitched,
+                    serieJobsSwitched, serieJobsNoSwitched, serieResultsSwiched, serieResultsNoSwitched);
         }
-        
-        System.out.print("Creo");
+
     }
 }
