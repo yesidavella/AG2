4927c59868f5b41a2d3efb7f0e3d398f102e345f
franklinalwinmartinez@gmail.com <franklinalwinmartinez@gmail.com@2d66d220-3702-eadc-dac1-119fe40fcf4a>
2012-11-22 04:48:23 +0000

    Cambio de propiedades

--- trunk/edu/ag2/config/PhosphorusPropertyEditor.java
+++ trunk/edu/ag2/config/PhosphorusPropertyEditor.java
@@ -11,8 +11,34 @@ public final class PhosphorusPropertyEditor {
     private Properties properties = new Properties();
     private static PhosphorusPropertyEditor phosphorusPropertyEditor;
 
-    private PhosphorusPropertyEditor() {
-        readFile();
+    private PhosphorusPropertyEditor() 
+    {
+        
+        properties.setProperty("stopEventOffSetTime", "100");
+        properties.setProperty("defaultWavelengths", "253");
+        properties.setProperty("maxDelay", "10");
+        properties.setProperty("clientTrafficPriority", "5");
+        properties.setProperty("defaultFlopSize", "500");
+        properties.setProperty("defaultResultSize", "200");
+        properties.setProperty("defaultDataSize", "10");
+        properties.setProperty("routedViaJUNG", "true");
+        properties.setProperty("defaultCPUCount", "3");
+        properties.setProperty("defaultJobIAT", "200");
+        properties.setProperty("output", "true");
+        properties.setProperty("allocateWavelenght", "0.01");
+        properties.setProperty("outputFileName", "resultado.html");
+        properties.setProperty("defaultQueueSize", "20");
+        properties.setProperty("simulationTime", "1000000");
+        properties.setProperty("switchingSpeed", "100000");
+        properties.setProperty("findCommonWavelenght", "0.03");
+        properties.setProperty("OCS_SwitchingDelay", "0.01"); 
+        properties.setProperty("confirmOCSDelay", "0.0001");
+        properties.setProperty("ACKsize", "0.1"); 
+        properties.setProperty("OCSSetupHandleTime", "0.5");
+        properties.setProperty("linkSpeed", "100");
+        properties.setProperty("OBSHandleTime", "10");
+        properties.setProperty("defaultCpuCapacity", "100");
+        
     }
 
     public static PhosphorusPropertyEditor getUniqueInstance() {
@@ -22,29 +48,30 @@ public final class PhosphorusPropertyEditor {
         return phosphorusPropertyEditor;
     }
 
-    public void readFile() {
-
-        try {
-            FileInputStream fileInputStream = new FileInputStream(Utils.ABS_PATH_CONFIG_AG2 + FILE_NAME);
-            properties.load(fileInputStream);
-            fileInputStream.close();
-
-        } catch (Exception ex) {
-            //System.out.println("Failed to read from " + Utils.ABS_PATH_CONFIG_AG2 + FILE_NAME + " file. Due to :" + ex.toString());
-        }
-    }
-
-    public void writeFile() {
-        
-        try {
-            FileOutputStream fileOutputStream = new FileOutputStream(Utils.ABS_PATH_CONFIG_AG2 + FILE_NAME);
-            properties.store(fileOutputStream, null);
-            fileOutputStream.flush();
-            fileOutputStream.close();
-        } catch (Exception ex) {
-            //System.out.println("Failed to write from " + Utils.ABS_PATH_CONFIG_AG2 + FILE_NAME + " file. Due to :" + ex.toString());
-        }
-    }
+//    public void readFile() {
+//
+//        try {
+//         
+//            FileInputStream fileInputStream = new FileInputStream(Utils.ABS_PATH_CONFIG_AG2 + FILE_NAME);
+//            properties.load(fileInputStream);
+//            fileInputStream.close();
+//
+//        } catch (Exception ex) {
+//            //System.out.println("Failed to read from " + Utils.ABS_PATH_CONFIG_AG2 + FILE_NAME + " file. Due to :" + ex.toString());
+//        }
+//    }
+//
+//    public void writeFile() {
+//        
+//        try {
+//            FileOutputStream fileOutputStream = new FileOutputStream(Utils.ABS_PATH_CONFIG_AG2 + FILE_NAME);
+//            properties.store(fileOutputStream, null);
+//            fileOutputStream.flush();
+//            fileOutputStream.close();
+//        } catch (Exception ex) {
+//            //System.out.println("Failed to write from " + Utils.ABS_PATH_CONFIG_AG2 + FILE_NAME + " file. Due to :" + ex.toString());
+//        }
+//    }
 
     public String getPropertyValue(PropertyPhosphorusTypeEnum propertyPhosphorusTypeEnum) {
         return properties.getProperty(propertyPhosphorusTypeEnum.getPhosphorusPropertyName());
@@ -52,7 +79,7 @@ public final class PhosphorusPropertyEditor {
 
     public void setPropertyValue(PropertyPhosphorusTypeEnum propiedadePhosphorus, String value) {
         properties.setProperty(propiedadePhosphorus.getPhosphorusPropertyName(), value);
-        writeFile();
+//        writeFile();
     }
 
     public Properties getProperties() {
